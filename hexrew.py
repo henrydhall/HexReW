48d8846ac1f8314189ae0ec76dea02ae58fb7a47
HenrydHall <henrydhall3.14@gmail.com>
2025-04-21 20:51:27 -0600

    refactoring adding functions to pvz_hex

--- hexrew.py
+++ hexrew.py
@@ -1,5 +1,6 @@
 """Some utilities for messing around with hexadecimal files."""
 
+
 def read_file(file_name) -> list:
     """Read a file and return it as a list."""
     with open(file_name, 'rb') as rf:
@@ -9,7 +10,7 @@ def read_file(file_name) -> list:
 
 
 def get_word(hex_list, offset) -> list[int]:
-    return hex_list[offset:offset+4:]
+    return hex_list[offset : offset + 4 :]
 
 
 def get_byte(hex_list, offset) -> int:
@@ -17,17 +18,17 @@ def get_byte(hex_list, offset) -> int:
 
 
 def hex_to_int(hex_word) -> int:
-    return int(''.join( [hex(byte).replace('0x','') for byte in hex_word[-1::-1]] ), base=16)
+    return int(''.join([hex(byte).replace('0x', '') for byte in hex_word[-1::-1]]), base=16)
 
 
 def create_file(filename, contents):
-    with open(filename,mode='xb') as wf:
+    with open(filename, mode='xb') as wf:
         contents = bytearray(contents)
         wf.write(contents)
 
 
 def over_write_file(filename, contents: int):
-    with open(filename,'wb') as wf:
+    with open(filename, 'wb') as wf:
         contents = bytearray(contents)
         wf.write(contents)
 
@@ -39,4 +40,4 @@ def set_byte(bytes, offset, value):
 if __name__ == '__main__':
     my_bytes = read_file('user1.dat')
     my_bytes[0] = 0x0D
-    over_write_file('hello.dat',my_bytes)
+    over_write_file('hello.dat', my_bytes)
--- pvz_hex.py
+++ pvz_hex.py
@@ -4,16 +4,41 @@ import hexrew
 import pvz_const
 
 
+class PvZParser:
+    """Plants vs. Zombies save file parser."""
+
+    def __init__(self, save_file):
+        self.file = hexrew.read_file(save_file)
+
+    def get_money(self):
+        """Return the amount of money in the save."""
+        return 10 * hexrew.hex_to_int(hexrew.get_word(self.file, pvz_const.MONEY))
+
+    def get_level(self):
+        """Get the current level."""
+        return hexrew.hex_to_int(hexrew.get_word(self.file, pvz_const.CURRENT_LEVEL))
+
+    def get_attribute(self, attribute):
+        """Get the attribute."""
+        return self.file[attribute]
+
+    def get_plant(self, n) -> str:
+        """Get the nth plant."""
+        return pvz_const.PLANTS[self.get_attribute(pvz_const.FIRST_PLANT + (pvz_const.PLANT_SIZE * n))]
+
+    def get_plant_garden(self, n) -> str:
+        """Get the nth plant's garden as a str."""
+        return pvz_const.GARDENS[
+            self.get_attribute(pvz_const.GARDEN_LOCATION + pvz_const.FIRST_PLANT + (pvz_const.PLANT_SIZE * n))
+        ]
+
+
 if __name__ == '__main__':
-    file = hexrew.read_file('user1.dat')
-    word = hexrew.get_word(file, pvz_const.MONEY)
-    print('Money:',10*hexrew.hex_to_int(hexrew.get_word(file, pvz_const.MONEY)))
-    print('Level:', hexrew.hex_to_int(hexrew.get_word(file, pvz_const.CURRENT_LEVEL) ))
-    print('Adventure completions:', hexrew.hex_to_int(hexrew.get_word(file, pvz_const.ADVENTURE_MODE) ))
-    print('Zen Garden Plants:', hexrew.hex_to_int(hexrew.get_word(file, pvz_const.ZEN_GARDEN_COUNT) ))
+    my_parser = PvZParser('user1.dat')
+    print('Money:', my_parser.get_money())
+    print('Level:', my_parser.get_level())
+    print('Adventure Completions:', my_parser.get_attribute(pvz_const.ADVENTURE_MODE))
+    print('Zen Garden Plants:', my_parser.get_attribute(pvz_const.ZEN_GARDEN_COUNT))
     print()
-    current_plant = pvz_const.FIRST_PLANT
-    while current_plant < len(file):
-        print( pvz_const.PLANTS[(hexrew.hex_to_int(hexrew.get_word(file, current_plant) ))])
-        print( (hexrew.hex_to_int(hexrew.get_word(file, current_plant + pvz_const.GARDEN_LOCATION))))
-        current_plant += pvz_const.PLANT_SIZE
+    for i in range(my_parser.get_attribute(pvz_const.ZEN_GARDEN_COUNT)):
+        print(my_parser.get_plant(i) + ':', my_parser.get_plant_garden(i))
